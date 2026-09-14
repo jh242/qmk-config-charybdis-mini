812da30f3bae597e374dc0f130600c95ff18a1fe
Jack Hu <34525842+jh242@users.noreply.github.com>
2026-09-14 10:49:54 -0400

    SYM: swap pipe and backslash (;=\, /=|)

--- keyboards/bastardkb/charybdis/3x6/keymaps/totem/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/totem/keymap.c
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_SYM] = LAYOUT(
        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-       _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TILD,     KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE, _______,
-       _______, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   _______,     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, KC_GRV,
+       _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TILD,     KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,
+       _______, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   _______,     KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_GRV,
                                   _______, _______, _______,     _______, _______
   ),
 
