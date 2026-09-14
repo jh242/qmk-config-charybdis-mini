7efd78673aebb400a14576c71eec4c911910610a
Jack Hu <34525842+jh242@users.noreply.github.com>
2026-09-14 09:01:48 -0400

    Fix NAV layer syntax in Totem keymap

--- keyboards/bastardkb/charybdis/3x6/keymaps/totem/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/totem/keymap.c
@@ -47,14 +47,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_NAV] = LAYOUT(
   // Numbers left, vim arrows / home-end-pg right (Totem NAV)
        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL,
-       _______,  _______, _______, _______, _______, _______,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT, _______,
-       _______,  _______, _______,C(KC_BSPC),_______, _______,    KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_GRV, _______/*,
-                                  _______, _______, _______,    _______, _______ */
-                                  _______, _______, _______,    _______, _______
+       _______,  _______, _______, _______, _______, _______,    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT, _______/*,
+       */,_______,  _______, _______,C(KC_BSPC),_______, _______,    KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_GRV, _______/*,
+                                  */,_______, _______, _______,    _______, _______
   ),
 
   [LAYER_SYM] = LAYOUT(
-  // Brackets / operators (Totem SYM)
        _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
        _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TILD,    KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, _______,
        _______, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   _______,    KC_UNDS, KC_PLUS, _______, _______, KC_BSLS,  KC_GRV,
@@ -62,7 +60,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_NUM] = LAYOUT(
-  // NAV+SYM tri-layer: F-keys + numpad. BT → boot/EEPROM/RGB (wired QMK).
        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_PAST,    KC_7,    KC_8,    KC_9, KC_PMNS, _______,
        _______, RM_TOGG, RM_NEXT, RM_PREV, RM_HUEU, RM_SATU,    KC_PSLS,    KC_4,    KC_5,    KC_6, KC_PPLS, _______,
        QK_BOOT,  EE_CLR, _______, _______,  KC_F11,  KC_F12,       KC_0,    KC_1,    KC_2,    KC_3,  KC_DOT, KC_BSPC,
@@ -70,7 +67,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_POINTER] = LAYOUT(
-  // Auto-mouse target (Voyager-like). Buttons on thumbs; DPI / snip / drag.
        QK_BOOT,  EE_CLR, _______, _______, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, _______, _______,  EE_CLR, QK_BOOT,
        _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,    _______, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, _______,
        _______, _______, DRGSCRL, SNIPING, _______, _______,    _______, _______, SNIPING, DRGSCRL, _______, _______,
@@ -79,7 +75,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-/* Combos — Totem: J+K Esc, Z+X caps-word */
 const uint16_t PROGMEM combo_jk_esc[] = {HRM_J, HRM_K, COMBO_END};
 const uint16_t PROGMEM combo_zx_caps[] = {KC_Z, KC_X, COMBO_END};
 
